94cc5c774601dfc5de5b415e765fda9e0b77dc14
Lee Duncan <lduncan@suse.com>
2026-01-31 08:00:11 -0800

    Add 'cwd' argument to run_command()
    Callers can pass in a directory to change to
    before executing the process specified.
    
    This will help for calling 'git', where we need
    to either be in the proper directory when calling it,
    or we need to pass in the "--git-dir" argument each
    time.

--- patchtools/command.py
+++ patchtools/command.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from subprocess import PIPE, run
 
 
-def run_command(command, our_input=None, stdout=PIPE):
+def run_command(command, our_input=None, stdout=PIPE, cwd=None):
     """Run a command, with optional input and output supplied."""
     with Path('/dev/null').open('wb') as dn:
         proc = run(command.split(), encoding='utf-8', input=our_input,
-                   stdout=stdout, stderr=dn, check=False)
+                   stdout=stdout, stderr=dn, check=False, cwd=cwd)
     return proc.stdout
