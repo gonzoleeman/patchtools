1ba9b7b3fde8990e8c7a398c912e1ad238594f73
Lee Duncan <lduncan@suse.com>
2026-02-02 09:28:22 -0800

    Test cleanup: remove unused test module.
    the test_patch.py module was intended to be used for
    tests focusing on patch.py/Patch(), but they haven't
    materialized, so remove the module and the lint
    warning that go with it.

--- test/__init__.py
+++ test/__init__.py
@@ -2,7 +2,6 @@
 
 from .test_exportpatch import TestExportpatchExclude, TestExportpatchExtract, TestExportpatchNormalFunctionality
 from .test_fixpatch import TestFixpatchErrorCases, TestFixpatchNormalFunctionality
-from .test_patch import TestPatchModuleNormalFunctionality
 
 __all__ = [
     'TestExportpatchExclude',
@@ -10,7 +9,6 @@ __all__ = [
     'TestExportpatchNormalFunctionality',
     'TestFixpatchErrorCases',
     'TestFixpatchNormalFunctionality',
-    'TestPatchModuleNormalFunctionality',
     ]
 
 # vim: sw=4 ts=4 et si:
